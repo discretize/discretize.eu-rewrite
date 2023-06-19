6a001d21c87e9133483d7657213d261e980e076c
gw2princeps <21026467+gw2princeps@users.noreply.github.com>
2023-06-19 23:50:08 +0200

    feat: improve components

--- src/components/components.ts
+++ src/components/components.ts
@@ -4,6 +4,13 @@ import Card from "./Card.astro";
 import GifPlayer from "./GifPlayer.astro";
 import Grid from "./Grid.astro";
 import GridItem from "./GridItem.astro";
+import Information from "./Information.astro";
+import Label from "./Label.astro";
+import Tab from "./Tab.astro";
+import Tabs from "./Tabs.astro";
+import TextDivider from "./TextDivider.astro";
+import Video from "./Video.astro";
+import Warning from "./Warning.astro";
 import Attribute from "./gw2-ui/Attribute.astro";
 import Augmentation from "./gw2-ui/Augmentation.astro";
 import Boon from "./gw2-ui/Boon.astro";
@@ -18,38 +25,31 @@ import Trait from "./gw2-ui/Trait.astro";
 import Traits from "./gw2-ui/Traits.astro";
 import Achievement from "./gw2/Achievement.astro";
 import BuildLink from "./gw2/BuildLink.astro";
-import Character from "./gw2/character/Character.astro";
 import CharacterWithAr from "./gw2/CharacterWithAr.astro";
+import Character from "./gw2/character/Character.astro";
+import Image from "./html-components/Image.astro";
+import MDLink from "./html-components/MDLink.astro";
 import blockquote from "./html-components/blockquote.astro";
 import code from "./html-components/code.astro";
 import hr from "./html-components/hr.astro";
-import Image from "./html-components/Image.astro";
-import MDLink from "./html-components/MDLink.astro";
+import li from "./html-components/li.astro";
+import ol from "./html-components/ol.astro";
+import pre from "./html-components/pre.astro";
 import table from "./html-components/table.astro";
 import tbody from "./html-components/tbody.astro";
-import td from "./html-components/td.astro";
 import th from "./html-components/th.astro";
 import tr from "./html-components/tr.astro";
 import ul from "./html-components/ul.astro";
-import Information from "./Information.astro";
-import Label from "./Label.astro";
-import Tab from "./Tab.astro";
-import Tabs from "./Tabs.astro";
-import TextDivider from "./TextDivider.astro";
-import Video from "./Video.astro";
-import Warning from "./Warning.astro";
-import li from "./html-components/li.astro";
-import ol from "./html-components/ol.astro";
 
 export const components = {
   hr,
-  td,
   tr,
   th,
   ul,
   li,
   ol,
   code,
+  pre,
   blockquote,
   a: MDLink,
   tbody,
