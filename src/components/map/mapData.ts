8dee6cf652f714173d2422950eff8b9ba4afd939
gw2princeps <21026467+gw2princeps@users.noreply.github.com>
2023-06-28 23:19:45 +0200

    feat: add mai trin

--- src/components/map/mapData.ts
+++ src/components/map/mapData.ts
@@ -1,8 +1,10 @@
+import { Placement } from "@floating-ui/react-dom";
 import snowblindImage from "../../assets/images/maps/snowblind.jpeg";
+import thaumnovaReactorIamge from "../../assets/images/maps/thaumanovaReactor.jpg";
 import SnowblindPathSVG from "./path-svg/Snowblind.astro";
 import ThaumanovaReactorSVG from "./path-svg/ThaumanovaReactor.astro";
-import thaumnovaReactorIamge from "../../assets/images/maps/thaumanovaReactor.jpg";
-import { Placement } from "@floating-ui/react-dom";
+import captainMaiTrinBossImage from "../../assets/images/maps/captainMaiTrinBoss.png";
+import CaptainMaiTrinBossPathSVG from "./path-svg/CaptainMaiTrinBoss.astro";
 
 export type Encounter = {
   xPercent: number;
@@ -99,6 +101,28 @@ const mapsData: Record<
         yPercent: 48,
         isMajor: true,
         area: '<ellipse ry="153.846" rx="158.46136" id="svg_17" cy="1499.84274" cx="1745.53448" />',
+      },
+    ],
+  },
+  "captain-mai-trin-boss": {
+    src: captainMaiTrinBossImage,
+    width: 2004,
+    height: 2264,
+    PathSVG: CaptainMaiTrinBossPathSVG,
+    encounters: [
+      {
+        xPercent: 25,
+        yPercent: 52,
+        isMajor: true,
+        tooltipDirection: "right",
+        area: '<ellipse ry="280.00001" rx="356.36364" cy="645.9862" cx="1124.84345" />',
+      },
+      {
+        xPercent: 62,
+        yPercent: 60,
+        isMajor: true,
+        tooltipDirection: "bottom",
+        area: '<ellipse ry="365.45455" rx="512.72728"  cy="1460.53168" cx="1152.11618" />',
       },
     ],
   },
