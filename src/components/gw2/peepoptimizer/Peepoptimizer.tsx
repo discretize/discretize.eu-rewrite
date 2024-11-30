e62032947f7e442435a7e48b90a070b6e17140ef
marcustyphoon <marcustyphoon@gmail.com>
2024-11-30 00:38:49 -0800

    chore: Update gear optimizer module (#32)
    * use updated optimizer api
    
    * update optimizer test state
    
    * update gear optimizer submodule

--- src/components/gw2/peepoptimizer/Peepoptimizer.tsx
+++ src/components/gw2/peepoptimizer/Peepoptimizer.tsx
@@ -5,10 +5,7 @@ import ResultCharacter from "./ResultCharacter";
 
 const testState = {
   optimizer: {
-    userSettings: {
-      expertMode: true,
-      gameMode: "fractals",
-    },
+    userSettings: { expertMode: true, gameMode: 'fractals' },
     control: {
       list: [],
       filteredLists: {
@@ -20,77 +17,58 @@ const testState = {
         Enhancement: [],
       },
       saved: [],
-      compareByPercent: false,
+      compareByPercent: true,
+      highlightDiffering: false,
       tallTable: false,
-      filterMode: "None",
+      savedHeader: false,
+      filterMode: 'None',
       displayAttributes: [],
       progress: 0,
       selectedCharacter: null,
-      selectedTemplate: "Quickness Condi Firebrand 77%",
-      status: "WAITING",
-      profession: "Guardian",
-      selectedSpecialization: "Firebrand",
+      selectedTemplate: 'Condi Virtuoso Dueling',
+      status: 'WAITING',
+      profession: 'Mesmer',
+      selectedSpecialization: 'Virtuoso',
+      jsHeuristicsEnabled: false,
+      jsHeuristicsTarget: '',
       multicore: false,
-      hwThreads: 10,
+      hwThreads: '',
       heuristics: false,
-      error: "",
+      error: '',
     },
     form: {
       extras: {
         extras: {
-          Sigil1: {
-            bursting: {},
-          },
-          Sigil2: {
-            smoldering: {},
-          },
-          Runes: {
-            trapper: {},
-            nightmare: {},
-            tempest: {},
-            lich: {},
-            balthazar: {},
-            renegade: {},
-            berserker: {},
-            weaver: {},
-            baelfire: {},
-            "flame-legion-2": {},
-            firebrand: {},
-          },
-          Relics: {},
-          Nourishment: {
-            "fishy-rice-bowl": {},
-          },
-          Enhancement: {
-            "toxic-focusing-crystal": {},
-            "magnanimous-tuning-crystal": {},
-            "toxic-maintenance-oil": {},
-            "magnanimous-maintenance-oil": {},
-          },
+          Sigil1: { agony: {} },
+          Sigil2: { earth: { amount: '4.3' } },
+          Runes: { krait: {} },
+          Relics: { 'aristocracy-relic': {} },
+          Nourishment: { 'fancy-potato-and-leek-soup': {} },
+          Enhancement: { 'toxic-focusing-crystal': {} },
         },
-        lifestealAmount: "",
+        lifestealAmount: '',
       },
       distribution: {
-        selectedDistribution: "Condi Quickbrand (WT, Allies)",
+        selectedDistribution: 'Condi Virtuoso Dueling',
         version: 2,
         values1: {},
         values2: {
-          Power: 1807,
-          Power2: 0,
-          Burning: 12.91,
-          Bleeding: 13.76,
+          Power: 3200,
+          Power2: 930,
+          Burning: 0,
+          Bleeding: 0,
           Poisoned: 0,
-          Torment: 0,
-          Confusion: 0,
+          Torment: 8.98,
+          Confusion: 3.39,
         },
         textBoxes: {
-          Power: 1807,
-          Power2: 0,
-          Burning: 12.91,
-          Bleeding: 13.76,
+          Power: 3200,
+          Power2: 930,
+          Burning: 0,
+          Bleeding: 0,
           Poisoned: 0,
-          Torment: 0,
-          Confusion: 0,
+          Torment: 8.98,
+          Confusion: 3.39,
         },
       },
       buffs: {
@@ -108,9 +86,9 @@ const testState = {
           frostSpirit: false,
           pinpointDistribution: false,
           strengthInNumbers: false,
-          "jade-bot-base": true,
-          "jade-bot-per-tier": true,
-          "reinforced-armor": true,
+          'jade-bot-base': true,
+          'jade-bot-per-tier': true,
+          'reinforced-armor': true,
           baneSignet: false,
           signetOfJudgment: false,
           signetOfMercy: false,
@@ -124,12 +102,12 @@ const testState = {
       },
       infusions: {
         omnipotion: true,
-        ar: "162",
-        maxInfusions: "18",
-        primaryInfusion: "",
-        secondaryInfusion: "",
-        primaryMaxInfusions: "",
-        secondaryMaxInfusions: "",
+        ar: '162',
+        maxInfusions: '18',
+        infusionOptions: [
+          { type: '', count: '' },
+          { type: '', count: '' },
+        ],
         helperData: {
           enabled: false,
           slots: 18,
@@ -143,105 +121,77 @@ const testState = {
       },
       traits: {
         showAll: false,
-        selectedLines: ["16", "46", "62"],
+        selectedLines: ['24', '1', '66'],
         selectedTraits: [
-          [566, 567, 1686],
-          [617, 587, 622],
-          [2075, 2063, 2179],
+          [721, 1690, 733],
+          [701, 708, 692],
+          [2202, 2205, 2223],
         ],
         items: [
           {
-            "right-hand-strength": {},
-            "right-hand-strength-1h": {
-              amount: "100",
-            },
-            "radiant-fire": {},
-            retribution: {
-              amount: "",
-            },
-            "radiant-power": {},
-            "amplified-wrath": {},
-            "righteous-instincts": {},
+            'compounding-power': { amount: '' },
+            'Phantasmal Force': { amount: '' },
           },
           {
-            "inspired-virtue": {
-              amount: "",
-            },
-            "unscathed-contender": {
-              amount: "",
-            },
-            "virtue-of-resolution": {},
-            "inspiring-virtue": {
-              amount: "",
-            },
-            "power-of-the-virtuous": {},
+            'fencers-finesse': { amount: '5.24' },
+            'superiority-complex-base': {},
+            'superiority-complex': { amount: '' },
+            'sharper-images-phantasm': { amount: '1.8' },
+            'phantasmal-fury-virtuoso': {},
           },
           {
-            "imbued-haste": {
-              amount: "",
-            },
+            'mental-focus': { amount: '' },
+            'deadly-blades': { amount: '100' },
+            'sharpening-sorrow': {},
+            'quiet-intensity': {},
+            'quiet-intensity-fury': {},
+            bloodsong: {},
+            'jagged-mind': { amount: '6.04' },
           },
         ],
       },
       skills: {
-        skills: {
-          "mantra-of-flame": {},
-        },
+        skills: { 'signet-of-domination': {}, 'signet-of-midnight': {} },
       },
       priorities: {
-        optimizeFor: "Damage",
-        weaponType: "Dual wield",
-        minBoonDuration: 0,
-        minHealingPower: "",
-        minToughness: "",
-        maxToughness: "",
-        minHealth: "",
-        minCritChance: "",
-        minDamage: "",
-        minHealing: "",
-        minSurvivability: "",
-        minOutgoingHealing: "",
-        minQuicknessDuration: 77.4,
-        affixes: ["Viper", "Ritualist"],
-        exclusions: {
-          enabled: false,
-          data: {},
-        },
-        exotics: {
-          enabled: false,
-          data: {},
-        },
+        optimizeFor: 'Damage',
+        weaponType: 'Dual wield',
+        minBoonDuration: '0',
+        minHealingPower: '',
+        minToughness: '',
+        maxToughness: '',
+        minHealth: '',
+        minCritChance: '',
+        minDamage: '',
+        minHealing: '',
+        minSurvivability: '',
+        minOutgoingHealing: '',
+        minQuicknessDuration: '0',
+        affixes: ['Viper', 'Sinister', 'Rampager'],
+        exclusions: { enabled: false, data: {} },
+        exotics: { enabled: false, data: {} },
         customAffix: {},
-        customAffixTextBox: "",
-        customAffixError: "",
+        customAffixTextBox: '',
+        customAffixError: '',
       },
-      extraModifiers: {
-        error: "",
-        extraModifiers: [],
-        textBox: "",
-      },
+      extraModifiers: { error: '', extraModifiers: [], textBox: '' },
       forcedSlots: {
-        slots: [
-          null,
-          null,
-          null,
-          null,
-          null,
-          null,
-          null,
-          null,
-          null,
-          null,
-          null,
-          null,
-          null,
-          null,
-        ],
+        slots: [null, null, null, null, null, null, null, null, null, null, null, null, null, null],
       },
-      boss: {
-        attackRate: "0.4",
-        movementUptime: "0",
+      boss: { attackRate: '0.4', movementUptime: '0' },
+    },
+    buildPage: {
+      character: null,
+      weapons: { mainhand1: '', offhand1: '', mainhand2: '', offhand2: '' },
+      skills: {
+        healId: '',
+        utility1Id: '',
+        utility2Id: '',
+        utility3Id: '',
+        eliteId: '',
       },
+      traits: { lines: [], selected: [] },
+      buffs: 0,
     },
   },
 };
@@ -266,23 +216,22 @@ const Component = ({ templates }) => {
   ]);
 
   async function calc() {
-    let value,
-      done = false;
-
     const { calculate } = await import(
       "discretize-gear-optimizer/src/state/optimizer/optimizer"
     );
 
     testState.optimizer.form.forcedSlots.slots = forcedSlots;
-    const resultGenerator = calculate(testState);
-    while (true) {
-      ({ value, done } = resultGenerator.next());
-      if (done) {
-        break;
+    const resultGenerator = calculate(testState, 1);
+
+    let result = [];
+
+    for await (const { percent, list } of resultGenerator) {
+      if (list) {
+        result = list;
       }
     }
-    setResult(value.list);
-    console.log(value);
+
+    setResult(result);
   }
 
   const onClick = () => {
